5a9ddefa69fc828b5170cd24155895294b5b5464
KonstantIMP <mihedovkos@gmail.com>
2021-11-03 10:52:47 +0300

    Add supported bank services

---
+++ src/org/kimp/TicketType.java
@@ -0,0 +1,12 @@
+package org.kimp;
+
+/**
+ * Supported bank operations
+ */
+public enum TicketType {
+    CREDIT,     // Credit services
+    CARD,       // Credit and debit cards
+    PAYMENT,    // Making payments
+    BUSINESS,   // Business services
+    DOCUMENT    // Getting documents
+}
